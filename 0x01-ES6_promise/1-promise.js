4f937caac2c6d8a547bfe7a4e5a3c8e2ab23f55a
y4h14 <yahyasadig2013@gmail.com>
2024-05-29 22:43:28 +0300

    updated task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,11 +3,11 @@
 export default function getFullResponseFromAPI(success){
   return new Promise((resolve, reject) => {
     if (success === true){
-	  resolve({ status: 200, body: 'Success' }) }
-    else {
-	reject('The fake API is not working currently')
+	  resolve({ status: 200, body: 'Success' });
+    } else {
+	reject(new Error('The fake API is not working currently'));
     }
 
-  })
+  });
 
 }
