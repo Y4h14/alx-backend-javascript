efbac76efdb8828073c6e249209dc5aa572463c1
y4h14 <yahyasadig2013@gmail.com>
2024-05-30 23:51:22 +0300

    Added tasks 2 to 10

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+    get code() {
+        return this._code;
+    }
+    get name() {
+        return this._name;
+    }
+    set code(code) {
+        if (typeof code !== 'string') {
+            throw new TypeError('code must be a string');
+        }
+        this._code = code;
+    }
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('code must be a string');
+        }
+        this._name = name
+    }
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+
+}
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount
+        this._currency = currency
+    }
+    get amount() {
+        return this._amount;
+    }
+    get currency() {
+        this._currency;
+    }
+    set amount(amount) {
+        if(typeof amount !== 'number'){
+            throw new TypeError('amoutn must be a string');
+        }
+        this._amount = amount;
+    }
+    set currency(currency) {
+        if (typeof currency !== 'Currency') {
+            throw new TypeError('currency must be a currency');
+        }
+        this._currency = currency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency['name']} (${this._currency['code']})`
+    }
+
+    convertPrice(amount, conversionReate) {
+        return amount * conversionReate;
+    }
+
+}
