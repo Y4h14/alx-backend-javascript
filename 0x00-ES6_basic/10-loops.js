ba915ec667941d3871d63c7a7130c0a5775301e8
Y4h14 <yahyasadig2013@gmail.com>
2024-05-31 16:09:27 +0300

    Added tasks 1 to 12

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  for (const item of array) {
+    result.push(appendString + item);
+  }
+  return result;
+}
---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    departmentName: [
+      ...employees,
+    ],
+  };
+}
