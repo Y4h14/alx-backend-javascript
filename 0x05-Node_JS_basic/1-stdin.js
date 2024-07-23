a6eec7f279975408a997ee162f8d34cfb301ce0b
Y4h14 <yahyasadig2013@gmail.com>
2024-07-23 14:30:48 +0300

    fixed a typo in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdout.write('Welcome to Holberton School, what is our name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk) {
-    process.stdout.write(`Your name is ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
