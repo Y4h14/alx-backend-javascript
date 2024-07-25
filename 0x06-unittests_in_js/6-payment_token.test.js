97cd1a174abf1358809faf52f85330718e6d5151
Y4h14 <yahyasadig2013@gmail.com>
2024-07-25 14:56:35 +0300

    updated task e6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,10 +3,14 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', (done) => {
+  // eslint-disable-next-line jest/prefer-expect-assertions
   it('test with true', () => {
+    // eslint-disable-next-line jest/valid-expect-in-promise
     getPaymentTokenFromAPI(true)
       .then((res) => {
+        // eslint-disable-next-line jest/valid-expect
         expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        done();
       });
   });
 });
