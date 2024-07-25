9478d99bdf6aaaae892912719d1163b680b4aa90
Y4h14 <yahyasadig2013@gmail.com>
2024-07-25 17:39:20 +0300

    added task 6

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen('1245', 'localhost', () => {
+  console.log('listening on port 1245');
+});
+
+module.exports = app;
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
