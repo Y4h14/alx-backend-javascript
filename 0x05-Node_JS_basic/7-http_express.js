a338700fc54225603653ee597ee82df62802992c
Y4h14 <yahyasadig2013@gmail.com>
2024-07-26 16:46:10 +0300

    fixed spacing in task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const express = require('express');
 const fs = require('fs').promises;
-// const countStudents = require('./3-read_file_async');
 
 const app = express();
 const PORT = 1245;
@@ -12,7 +11,7 @@ app.get('/', (_, res) => {
 });
 
 app.get('/students', async (_, res) => {
-  let responseText = 'Hello Holberton School!\n';
+  let responseText = 'This is the list of our students\n';
   try {
     const data = await fs.readFile(databasePath, 'utf8');
     const fileLines = data.trim().split('\n').filter((line) => line.trim() !== '');
@@ -33,12 +32,16 @@ app.get('/students', async (_, res) => {
       }
       fields[field].push(firstname);
     });
-
+    let lineCount = 0;
     for (const [field, firstnames] of Object.entries(fields)) {
-      responseText += `Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}\n`;
+      responseText += `Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`;
+      if (lineCount == 0) {
+        responseText += '\n';
+        lineCount += 1;
+      }
     }
   } catch (err) {
-    console.error('Cannot load the database');
+    responseText += 'Cannot load the database';
   }
   res.send(responseText);
 });
